ad4f73f71410675fa09e0a6e92c81c73d82d2d0d
GitHub Action <action@github.com>
2021-11-28 18:01:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/4dahalibut/textflowers/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/4dahalibut/textflowers/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.4dahalibut.textflowers.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.4dahalibut.textflowers.MyBundle
 
 class MyApplicationService {
 
